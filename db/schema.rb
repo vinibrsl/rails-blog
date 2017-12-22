da7823e7b656a823559d07e8fd5d2177b572b52a
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-12-22 09:04:23 -0200

    fix migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220183924) do
+ActiveRecord::Schema.define(version: 20171222105131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
