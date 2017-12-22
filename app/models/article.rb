c2bbcfa09142f1a4984997a227f6bbae3a62cc95
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-12-22 09:57:49 -0200

    add user-article 1-M association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-
 end
