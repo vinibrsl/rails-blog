01fcac64045463f62679902b790878dcd454eed4
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-12-26 23:06:50 -0200

    create users#destroy method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { email.downcase! }
   has_secure_password
 
